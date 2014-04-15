66d82472a01630407469339ee21ebe3c4794c7e4
namlook <nicolas.clairon@elkorado.com>
2014-04-15 17:59:37 +0200

    implement model and database

--- index.js
+++ index.js
@@ -1,87 +1,154 @@
 // var Ember = require('ember');
 
-App = Ember.Application.create({
-    ready: function() {
-        Ember.$('title').text(this.get('config').name);
-        console.log(this.get('config').name, 'is ready !');
-    }
-});
+Eurekapp = (function(clientConfig){
 
-App.Router.map(function() {
-    this.resource('type', { path: '/' }, function() {
-        this.route('show', {path: '/:type/:id'});
-        this.route('edit', {path: '/:type/:id/edit'});
-        this.route('new', {path: '/:type/new'});
-        this.route('list', {path: '/:type'});
+    var App = Ember.Application.create({
+        ready: function() {
+            Ember.$('title').text(this.get('config').name);
+            console.log(this.get('config').name, 'is ready !');
+        }
     });
-});
 
-App.ApplicationRoute = Ember.Route.extend({
-    model: function() {
-        return App;
-    }
-});
+    App.Router.map(function() {
+        this.resource('type', { path: '/' }, function() {
+            this.route('show', {path: '/:type/:id'});
+            this.route('edit', {path: '/:type/:id/edit'});
+            this.route('new', {path: '/:type/new'});
+            this.route('list', {path: '/:type'});
+        });
+    });
 
-App.ApplicationController = Ember.Controller.extend({
-    modelNames: function() {
-        var _modelNames = [];
-        for (var modelName in App.config.schemas){
-            _modelNames.push({classified: modelName.camelize().capitalize(), underscored: modelName.underscore()});
+    App.ApplicationConfig = Ember.Object.extend({
+        modelNames: function() {
+            var _modelNames = [];
+            for (var modelName in App.config.schemas){
+                _modelNames.push({classified: modelName.camelize().capitalize(), underscored: modelName.underscore()});
+            }
+            return _modelNames;
+        }.property('schemas')
+    });
+
+    App.ApplicationRoute = Ember.Route.extend({
+        model: function() {
+            return App.config;
         }
-        return _modelNames;
-    }.property('App.config.schemas')
-});
+    });
+
+    App.ApplicationController = Ember.ObjectController.extend({
+    });
 
 
-App.TypeListRoute = Ember.Route.extend({
-    model: function(params) {
-        return {type: params.type, results: ['TODO']};
-        // var type = _.str.classify(params.type);
-        // return new Ember.RSVP.Promise(function(resolve, reject) {
-        //     App.db[type].find({}, function(err, data) {
-        //         if(err) {
-        //             return reject(err);
-        //         }
-        //         else{
-        //             var objects = [];
-        //             data.forEach(function(item) {
-        //                 objects.push(item.toJSONObject());
-        //             });
-        //             var model = {type: params.type, objects: objects, data:data};
-        //             resolve(model);
-        //         }
-        //     });
-        // });
-    },
-    renderTemplate: function(controller, model) {
-        var template = 'type/list';
-        console.log(model);
-        if (Ember.TEMPLATES[model.type+'/list']) {
-            template = model.type+'/list';
+    App.TypeListRoute = Ember.Route.extend({
+        model: function(params) {
+            return this.get('db')[params.type.camelize().capitalize()].find();
+        },
+        renderTemplate: function(controller, model) {
+            var template = 'type/list';
+            if (Ember.TEMPLATES[model.type+'/list']) {
+                template = model.type+'/list';
+            }
+            return this.render(template);
         }
-        return this.render(template);
-    }
-});
+    });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
-});
 
-// Ember.TEMPLATES = require('./templates');
+    // Ember.TEMPLATES = require('./templates');
 
-// module.exports = (function(config){
-Eurekapp = (function(config){
+
+    /**** Models *****/
+    App.Model = Ember.ObjectProxy.extend({
+        _modelType: null,
+        content: null,
+        _values: function() {
+            var _results = Ember.A();
+            for (var fieldName in this.get('content')) {
+                var value = this.get(fieldName);
+                if(value) {
+                    _results.push({name: fieldName, value: value});
+                }
+            }
+            return _results.sortBy('name');
+        }.property('content')
+    });
+
+    App.ResultSet = Ember.ArrayProxy.extend({
+        type: null,
+        schema: null,
+        fields: function() {
+            var _fields = Ember.A();
+            for (var fieldName in this.get('schema')) {
+                _fields.push({name: fieldName, structure: this.get('schema')[fieldName]});
+            }
+            return _fields;
+        }.property('schema'),
+        content: null
+    });
+
+    App.DatabaseModel = Ember.Object.extend({
+        type: null,
+        schema: null,
+        model: null,
+
+        endpoint: function() {
+            return App.config.apiURI+'/'+this.get('type').underscore();
+        }.property('App.config.apiURI', 'type'),
+
+        find: function() {
+            var that = this;
+            var modelType = this.get('type');
+            return new Ember.RSVP.Promise(function(resolve, reject) {
+                Ember.$.getJSON(that.get('endpoint'), function(data){
+                    var results = Ember.A();
+                    data.results.forEach(function(item){
+                        var obj = that.get('model').create({
+                            content: item,
+                            _modelType: modelType
+                        });
+                        results.push(obj);
+                    });
+                    var resultSet = App.ResultSet.create({
+                        type: modelType,
+                        schema: that.get('schema'),
+                        content: results
+                    });
+                    return resolve(resultSet);
+                });
+            });
+        }
+    });
+
+
+
+    /**** Initialization *****/
+    // attach the config, and the db to the application
     App.initializer({
-        name: "oreka",
+        name: "eureka",
 
         initialize: function(container, application) {
-            application.register('oreka:config', config, {instantiate: false});
-            application.inject('route', 'config', 'oreka:config');
-            application.set('config', config);
-
+            application.register('eureka:config', App.ApplicationConfig.create(clientConfig), {instantiate: false});
+            application.inject('route', 'config', 'eureka:config');
+            application.set('config', App.ApplicationConfig.create(clientConfig));
+            var database = (function() {
+                var db = Ember.Object.create();
+                for (var _type in clientConfig.schemas) {
+                    var typeObject = App.DatabaseModel.create({
+                        type: _type,
+                        schema: clientConfig.schemas[_type].schema
+                    });
+                    if (App[_type+'Model']) {
+                        typeObject.set('model', App[_type+'Model']);
+                    } else {
+                        typeObject.set('model', App.Model);
+                    }
+                    db.set(_type, typeObject);
+                }
+                return db;
+            })();
+            application.register('eureka:db', database, {instantiate: false});
+            application.inject('route', 'db', 'eureka:db');
+            application.set('db', database);
         }
+
     });
     return App;
 });
