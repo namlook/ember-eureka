8b6d59335d838232a3dd7eeb33b705d3c780b51d
namlook <nicolas.clairon@elkorado.com>
2014-04-23 17:12:58 +0200

    implement title/image/description on the model

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ Eurekapp = (function(clientConfig){
 
     App.Router.map(function() {
         this.resource('type', { path: '/' }, function() {
-            this.route('show', {path: '/:type/:id'});
+            this.route('display', {path: '/:type/:id'});
             this.route('edit', {path: '/:type/:id/edit'});
             this.route('new', {path: '/:type/new'});
             this.route('list', {path: '/:type'});
@@ -52,13 +52,76 @@ Eurekapp = (function(clientConfig){
     });
 
 
+    App.TypeDisplayRoute = Ember.Route.extend({
+        model: function(params) {
+            var _type = params.type.camelize().capitalize();
+            var _id = params.id;
+            return this.get('db')[_type].first({_id: _id, _type: _type});
+        },
+        renderTemplate: function(controller, model) {
+            var template = 'type/display';
+            if (Ember.TEMPLATES[model.type+'/display']) {
+                template = model.type+'/display';
+            }
+            return this.render(template);
+        }
+    });
+
     // Ember.TEMPLATES = require('./templates');
 
 
     /**** Models *****/
     App.Model = Ember.ObjectProxy.extend({
         _modelType: null,
-        content: null,
+        // content: null,
+
+        title: function() {
+            var modelConfig = App.config.schemas[this._modelType];
+            if (modelConfig && modelConfig.display && modelConfig.display.generic && modelConfig.display.generic.title) {
+                var field = modelConfig.display.generic.title.replace('@', '.');
+                return this.get('content.'+field);
+            }
+            else if (this.get('content.title')) {
+                var _title = this.get('content.title');
+                if (typeof(_title) === 'string') {
+                    return _title;
+                }
+                else if (_title.en) {
+                    return _title.en;
+                }
+            }
+            return this.get('_id');
+        }.property('content.title', '_id'),
+
+        description: function() {
+            var modelConfig = App.config.schemas[this._modelType];
+            if (modelConfig && modelConfig.display && modelConfig.display.generic && modelConfig.display.generic.description) {
+                var descriptionSchema = modelConfig.display.generic.description;
+                if (typeof(descriptionSchema) === 'function') {
+                    return descriptionSchema(this);
+                }
+                var field = descriptionSchema.replace('@', '.');
+                return this.get('content.'+field);
+            }
+            return this.get('content.description');
+        }.property('content'),
+
+
+        image: function() {
+            var modelConfig = App.config.schemas[this._modelType];
+            if (modelConfig && modelConfig.display && modelConfig.display.generic && modelConfig.display.generic.image) {
+                var imgSchema = modelConfig.display.generic.image;
+                if (typeof(imgSchema) === 'function') {
+                    console.log(imgSchema(this));
+                    return imgSchema(this);
+                }
+                var field = imgSchema.replace('@', '.');
+                return this.get('content.'+field);
+            }
+            return this.get('content.image');
+        }.property('content'),
+
+
         _values: function() {
             var _results = Ember.A();
             for (var fieldName in this.get('content')) {
@@ -93,11 +156,34 @@ Eurekapp = (function(clientConfig){
             return App.config.apiURI+'/'+this.get('type').underscore();
         }.property('App.config.apiURI', 'type'),
 
-        find: function() {
+        isRelation: function(fieldName) {
+            var field = this.get('schema')[fieldName];
+            if (field) {
+                return !!App.db[field.type];
+            }
+            return false;
+        },
+
+        isMulti: function(fieldName) {
+            return !!this.get('schema')[fieldName].multi;
+        },
+
+        isI18n: function(fieldName) {
+            return !!this.get('schema')[fieldName].i18n;
+        },
+
+        getFieldType: function(fieldName) {
+            var field = this.get('schema')[fieldName];
+            if (field) {
+                return field.type;
+            }
+        },
+
+        find: function(query) {
             var that = this;
             var modelType = this.get('type');
             return new Ember.RSVP.Promise(function(resolve, reject) {
-                Ember.$.getJSON(that.get('endpoint'), function(data){
+                Ember.$.getJSON(that.get('endpoint'), query, function(data){
                     var results = Ember.A();
                     data.results.forEach(function(item){
                         var obj = that.get('model').create({
@@ -114,9 +200,92 @@ Eurekapp = (function(clientConfig){
                     return resolve(resultSet);
                 });
             });
+        },
+
+        first: function(query) {
+            var that = this;
+            var modelType = this.get('type');
+            query._populate = true;
+            return new Ember.RSVP.Promise(function(resolve, reject) {
+                Ember.$.getJSON(that.get('endpoint'), query, function(data){
+                    var obj;
+
+                    // if there is a match, we wrap all relations with Model objects
+                    if (data.results.length > 0) {
+                        var content = data.results[0];
+
+                        // for each field, check if it is a relation to wrap
+                        for (var key in content) {
+
+                            var value = content[key];
+                            var type = that.getFieldType(key);
+
+                            if (that.isRelation(key)) {
+
+                                if (that.isMulti(key)) {
+
+                                    var values = [];
+
+                                    value.forEach(function(item) {
+                                        var rel = App.db[type].get('model').create(item);
+                                        values.push(rel);
+                                    });
+
+                                    content[key] = values;
+                                }
+                                else {
+                                    content[key] = App.db[type].get('model').create(value);
+                                    console.log(type, App.db[type].get('model'), value, content[key], content[key].title);
+                                }
+                            }
+                        }
+
+                        // build the model
+                        obj = that.get('model').create({
+                            content: content,
+                            _modelType: modelType
+                        });
+                    }
+                    else {
+                        obj = that.get('model').create({
+                            content: {},
+                            _modelType: modelType
+                        });
+                    }
+                    return resolve(obj);
+                });
+            });
         }
     });
 
+
+    /*** Components ****/
+    App.DisplayFieldComponent = Ember.Component.extend({
+        value: null,
+
+        isArray: function() {
+            return Ember.isArray(this.get('value'));
+        }.property('value'),
+
+        isRelation: function() {
+            var value = this.get('value');
+            if (value._type !== undefined && value._id !== undefined) {
+                return true;
+            }
+            return false;
+        }.property('value')
+    });
+
+    /**** Handlebars helpers ****/
+    Ember.Handlebars.helper('titleize', function(value, options) {
+        if (value && value.get && value.get('title') !== undefined) {
+            return value.get('title');
+        }
+        else if (value._id !== undefined) {
+            return value._id;
+        }
+        return value;
+    });
 
 
     /**** Initialization *****/
--- templates.js
+++ templates.js
@@ -54,6 +54,116 @@ function program4(depth0,data) {
   return buffer;
   
 });
+Ember.TEMPLATES["components/display-field"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, hashTypes, hashContexts, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, self=this;
+
+function program1(depth0,data) {
+  
+  var buffer = '', stack1, hashTypes, hashContexts;
+  data.buffer.push("\n    <ul>\n        ");
+  hashTypes = {};
+  hashContexts = {};
+  stack1 = helpers.each.call(depth0, "val", "in", "value", {hash:{},inverse:self.noop,fn:self.program(2, program2, data),contexts:[depth0,depth0,depth0],types:["ID","ID","ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n    </ul>\n");
+  return buffer;
+  }
+function program2(depth0,data) {
+  
+  var buffer = '', stack1, hashTypes, hashContexts;
+  data.buffer.push("\n            <li>\n            ");
+  hashTypes = {};
+  hashContexts = {};
+  stack1 = helpers['if'].call(depth0, "val._id", {hash:{},inverse:self.program(6, program6, data),fn:self.program(3, program3, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n            </li>\n        ");
+  return buffer;
+  }
+function program3(depth0,data) {
+  
+  var buffer = '', stack1, stack2, hashTypes, hashContexts, options;
+  data.buffer.push("\n                ");
+  hashTypes = {};
+  hashContexts = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(4, program4, data),contexts:[depth0,depth0,depth0],types:["STRING","ID","ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers['link-to'] || (depth0 && depth0['link-to'])),stack1 ? stack1.call(depth0, "type.display", "val._type", "val._id", options) : helperMissing.call(depth0, "link-to", "type.display", "val._type", "val._id", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("\n            ");
+  return buffer;
+  }
+function program4(depth0,data) {
+  
+  var stack1, hashTypes, hashContexts, options;
+  hashTypes = {};
+  hashContexts = {};
+  options = {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  data.buffer.push(escapeExpression(((stack1 = helpers.titleize || (depth0 && depth0.titleize)),stack1 ? stack1.call(depth0, "val", options) : helperMissing.call(depth0, "titleize", "val", options))));
+  }
+
+function program6(depth0,data) {
+  
+  var buffer = '', hashTypes, hashContexts;
+  data.buffer.push("\n                ");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "val", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("\n            ");
+  return buffer;
+  }
+
+function program8(depth0,data) {
+  
+  var buffer = '', stack1, hashTypes, hashContexts;
+  data.buffer.push("\n    ");
+  hashTypes = {};
+  hashContexts = {};
+  stack1 = helpers['if'].call(depth0, "isRelation", {hash:{},inverse:self.program(12, program12, data),fn:self.program(9, program9, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n");
+  return buffer;
+  }
+function program9(depth0,data) {
+  
+  var buffer = '', stack1, stack2, hashTypes, hashContexts, options;
+  data.buffer.push("\n        ");
+  hashTypes = {};
+  hashContexts = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(10, program10, data),contexts:[depth0,depth0,depth0],types:["STRING","ID","ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers['link-to'] || (depth0 && depth0['link-to'])),stack1 ? stack1.call(depth0, "type.display", "value._type", "value._id", options) : helperMissing.call(depth0, "link-to", "type.display", "value._type", "value._id", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("\n    ");
+  return buffer;
+  }
+function program10(depth0,data) {
+  
+  var hashTypes, hashContexts;
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "value.title", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  }
+
+function program12(depth0,data) {
+  
+  var buffer = '', hashTypes, hashContexts;
+  data.buffer.push("\n        ");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "value", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("\n    ");
+  return buffer;
+  }
+
+  data.buffer.push("\n");
+  hashTypes = {};
+  hashContexts = {};
+  stack1 = helpers['if'].call(depth0, "isArray", {hash:{},inverse:self.program(8, program8, data),fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n");
+  return buffer;
+  
+});
 Ember.TEMPLATES["index"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
@@ -79,30 +189,90 @@ function program1(depth0,data) {
   return buffer;
   
 });
-Ember.TEMPLATES["type/list"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+Ember.TEMPLATES["type/display"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
-  var buffer = '', stack1, hashTypes, hashContexts, escapeExpression=this.escapeExpression, self=this;
+  var buffer = '', stack1, hashTypes, hashContexts, escapeExpression=this.escapeExpression, helperMissing=helpers.helperMissing, self=this;
 
 function program1(depth0,data) {
   
-  var buffer = '', hashTypes, hashContexts;
+  var buffer = '', stack1, hashTypes, hashContexts, options;
+  data.buffer.push("\n            <dt>");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "value.name", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("</dt>\n            <dd>\n                ");
+  hashContexts = {'value': depth0};
+  hashTypes = {'value': "ID"};
+  options = {hash:{
+    'value': ("value.value")
+  },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  data.buffer.push(escapeExpression(((stack1 = helpers['display-field'] || (depth0 && depth0['display-field'])),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "display-field", options))));
+  data.buffer.push("\n            </dd>\n        ");
+  return buffer;
+  }
+
+  data.buffer.push("\n<fieldset>\n    <legend>");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "model.type", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("</legend>\n    <h1>");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "model.title", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("</h1>\n    <dl>\n        ");
+  hashTypes = {};
+  hashContexts = {};
+  stack1 = helpers.each.call(depth0, "value", "in", "model._values", {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0,depth0,depth0],types:["ID","ID","ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n    </dl>\n</fieldset>\n");
+  return buffer;
+  
+});
+Ember.TEMPLATES["type/list"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, hashTypes, hashContexts, escapeExpression=this.escapeExpression, self=this, helperMissing=helpers.helperMissing;
+
+function program1(depth0,data) {
+  
+  var buffer = '', stack1, stack2, hashTypes, hashContexts, options;
   data.buffer.push("\n    <fieldset>\n        <legend>");
   hashTypes = {};
   hashContexts = {};
   data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "model.type", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
-  data.buffer.push("</legend>\n        ");
+  data.buffer.push("</legend>\n        <h2>");
   hashTypes = {};
   hashContexts = {};
-  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "item", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
-  data.buffer.push("\n        \n    </fieldset>\n");
+  options = {hash:{},inverse:self.noop,fn:self.program(2, program2, data),contexts:[depth0,depth0,depth0],types:["STRING","ID","ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers['link-to'] || (depth0 && depth0['link-to'])),stack1 ? stack1.call(depth0, "type.display", "model.type", "item._id", options) : helperMissing.call(depth0, "link-to", "type.display", "model.type", "item._id", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("</h2>\n        <p>");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "item.description", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("</p>\n        <img ");
+  hashContexts = {'src': depth0};
+  hashTypes = {'src': "ID"};
+  options = {hash:{
+    'src': ("item.image")
+  },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  data.buffer.push(escapeExpression(((stack1 = helpers['bind-attr'] || (depth0 && depth0['bind-attr'])),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "bind-attr", options))));
+  data.buffer.push(" />\n        \n\n        \n    </fieldset>\n");
   return buffer;
+  }
+function program2(depth0,data) {
+  
+  var hashTypes, hashContexts;
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "item.title", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
   }
 
   data.buffer.push("\n");
   hashTypes = {};
   hashContexts = {};
-  stack1 = helpers.each.call(depth0, "item", "in", "model.results", {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0,depth0,depth0],types:["ID","ID","ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  stack1 = helpers.each.call(depth0, "item", "in", "model", {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0,depth0,depth0],types:["ID","ID","ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
   return buffer;
   
