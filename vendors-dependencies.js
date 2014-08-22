b652420e1c5cc7458c64e9c5437fce890e733a19
namlook <nicolas.clairon@elkorado.com>
2014-08-22 15:58:52 +0200

    add vendors dependencies

---
+++ vendors-dependencies.js
@@ -0,0 +1,26 @@
+module.exports = {
+    js: [
+        "bower_components/jquery/dist/jquery.js",
+        "bower_components/handlebars/handlebars.js",
+        "bower_components/ember/ember.js",
+        "bower_components/bootstrap/dist/js/bootstrap.js",
+        "bower_components/pickadate/lib/legacy.js",
+        "bower_components/pickadate/lib/picker.js",
+        "bower_components/pickadate/lib/picker.date.js",
+        "bower_components/typeahead.js/dist/typeahead.bundle.js",
+        "bower_components/jQuery-Fakecrop/fakecrop/jquery.fakecrop.js",
+        "bower_components/jquery-textcomplete/jquery.textcomplete.js",
+        "bower_components/alertify.js/lib/alertify.js",
+        "bower_components/eurekapp/index.js",
+        "bower_components/eurekapp/dist/templates.js"
+    ],
+    css: [
+        "bower_components/normalize-css/normalize.css",
+        "bower_components/bootstrap/dist/css/bootstrap.css",
+        "bower_components/pickadate/lib/themes/default.css",
+        "bower_components/pickadate/lib/themes/default.date.css",
+        "bower_components/alertify.js/themes/alertify.core.css",
+        "bower_components/alertify.js/themes/alertify.default.css",
+        "bower_components/eurekapp/dist/eurekapp.css"
+    ]
+};
