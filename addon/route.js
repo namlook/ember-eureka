a93be0f3e53c4b38b20a530431c37db6b8a564c6
namlook <nicolas.clairon@elkorado.com>
2015-02-06 15:33:08 +0100

    render a generic template if no template is found for a specific view

--- addon/route.js
+++ addon/route.js
@@ -9,5 +9,31 @@ export default Ember.Route.extend({
     store: function() {
         var modelType = this.get('modelType');
         return this.db[modelType];
-    }.property('modelType')
+    }.property('modelType'),
+
+    name: function() {
+        return this.toString().split(':')[1].split('/').join('.');
+    }.property(),
+
+    routeType: function() {
+        return this.get('name').split('.')[1];
+    }.property('name'),
+
+    renderTemplate: function() {
+        this._super(this, arguments);
+
+        // if the template doesn't exists, we render the generic template
+        if (!this.container.resolve('template:'+path)) {
+
+            var routeType = this.get('routeType');
+            var dasherizedModelType = this.get('modelType').dasherize();
+            var path = this.get('name').split('.').join('/');
+
+            this.render('_widgets', {                     // render the template '_widgets'
+                into: dasherizedModelType+'.'+routeType,  // into this template (ex: `user.collection`)
+                // outlet: 'widgets',                     // the name of the outlet in that template
+                controller: path                          // the controller to use for the template
+            });
+        }
+    }
 });
