dbed0d9202ffb978fe34e601852ab3321043de86
namlook <nicolas.clairon@elkorado.com>
2015-02-11 15:08:34 +0100

    use the default controller in collection.index

--- app/controllers/generic/collection/index.js
+++ app/controllers/generic/collection/index.js
@@ -1,14 +1,3 @@
-import Controller from 'ember-eureka/controller';
+import DefaultController from './default';
 
-export default Controller.extend({
-
-    queryParams: ['query'],
-    query: null,
-
-    _queryUpdater: function() {
-        var query = this.get('model.query');
-        if (query) {
-            this.set('query', JSON.stringify(query));
-        }
-    }.observes('model.query')
-});
+export default DefaultController.extend({});
