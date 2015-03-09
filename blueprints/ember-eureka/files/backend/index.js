afe147cc7274cf782e75ceacb5211ee2e9d57d53
namlook <nicolas.clairon@elkorado.com>
2015-03-09 14:35:57 +0100

    fix renamed package import

--- blueprints/ember-eureka/files/backend/index.js
+++ blueprints/ember-eureka/files/backend/index.js
@@ -1,6 +1,6 @@
 /* jshint node: true */
 
-var EurekaServer = require('eurekapi');
+var EurekaServer = require('eurekajs');
 var config = require('../config/server');
 var server = new EurekaServer(config);
 server.start();
