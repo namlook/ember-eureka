6462d9f06c2536404d8533fbf056bcd42e6fabd8
namlook <nicolas.clairon@elkorado.com>
2015-03-24 15:45:32 +0100

    add model refresh to new controller

--- app/routes/eureka/generic/new.js
+++ app/routes/eureka/generic/new.js
@@ -4,6 +4,13 @@ export default Route.extend({
 
     model: function() {
         return this.get('store').createRecord();
+    },
+
+    actions: {
+        refreshModel: function() { // XXX only used by model-form
+            // this.refresh();
+            // return true; // bubble up to the model route
+        }
     }
 
 });
--- app/routes/eureka/generic/new/default.js
+++ app/routes/eureka/generic/new/default.js
@@ -6,6 +6,13 @@ export default Route.extend({
         var resource = this.get('resource');
         var modelRoute = this.container.resolve('eurekaResourceRoutes:new')[resource];
         return this.modelFor(modelRoute);
+    },
+
+    actions: {
+        refreshModel: function() { // XXX only used by model-form
+            // this.refresh();
+            return true; // bubble up to the model route
+        }
     }
 
 });
