2abfc44d4152215a51033d104c30e28f3619d6e6
namlook <nicolas.clairon@elkorado.com>
2015-03-24 15:37:09 +0100

    add delete action

--- app/controllers/eureka/generic/model.js
+++ app/controllers/eureka/generic/model.js
@@ -1,3 +1,16 @@
 import Controller from 'ember-eureka/controller';
 
-export default Controller.extend({});
+export default Controller.extend({
+
+    actions: {
+        delete: function(model) {
+            var that = this;
+            model.delete().then(function() {
+                that.send('toControllerAction', {
+                    name: 'transitionTo',
+                    payload: {routePath: model.get('meta.collectionIndexViewPath')}
+                });
+            });
+        }
+    }
+});
