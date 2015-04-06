7d16abfdd7a43e63e2f59ddb4e9b2b770fbddc26
namlook <nicolas.clairon@elkorado.com>
2015-04-06 15:44:46 +0200

    upgrade to ember 1.11.0 and use {{component}} helper instead of the dynamic-component addon

--- app/components/widget-property-number-form.js
+++ app/components/widget-property-number-form.js
@@ -1,12 +1,6 @@
 import Ember from 'ember';
 
-// TODO: use the following when ditching dynamic-component (pb with type)
-
-// export default Ember.TextField.extend({
-//     classNames: ['form-control'],
-//     type: 'number'
-// });
-
-export default Ember.Component.extend({
-    // classNames: ['form-control']
+export default Ember.TextField.extend({
+    classNames: ['form-control'],
+    type: 'number'
 });
